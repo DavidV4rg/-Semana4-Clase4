2d8e4f263f97626f2a9dcecfc9a6dca858c2ae49
Davidv4rg <id.vargas@uniandes.edu.co>
2019-06-30 20:58:11 +0000

    Se modifica el metodo para que 77 funcione

--- S4C4/factorial.cpp
+++ S4C4/factorial.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 int main()
 {
-    int n;
-    int factorial = 1;
-    cout << "Ingrese un numero: ";
-    cin >> n;
+    int n = 7;
+    double factorial = 1;
     for(int i = 1; i <=n; ++i)
     {
         factorial *= i;
     }
-    cout << "El factorial es "  << factorial<< endl;    
+    cout << "El factorial de 7 es: "  << factorial<< endl; 
+    
+    int e = 77;
+    for(int i = 1; i<= e; i++){
+        factorial *= i;
+    }
+    cout << "El factorial de 77 es: " << factorial << endl;
     return 0;
 }
+
