168c871ae69aa25cbc094c9b97387a39b6503537
davidv4rg <id.vargas@uniandes.edu.co>
2019-07-06 19:49:23 +0000

    Se modifica lo hecho en clase

--- S5C3/VargasDavid_S5C3_ODEs.cpp
+++ S5C3/VargasDavid_S5C3_ODEs.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void rungekutta(float *t, float *x, float*v, float dt);
+void rungekutta(float t_in, float x_in, floatv_in, float dt, float punt);
 //Función que halla la derivada de la velocidad
 
 float dvdt(float t, float x, float v)
@@ -20,32 +20,28 @@ float dxdt(float t, float x, float v)
 }
 
 //Método que resuelve las dos ecuaciones diferenciales al tiempo con Runge Kutta de 4orden
-void rungekutta(float *t, float *x, float*v, float dt)
+void rungekutta(float t_in, float x_in, floatv_in, float d, float punt)
 {
-    float t_in, x_in, v_in;
     float kx1, kx2, kx3, kx4;
     float kv1, kv2, kv3, kv4;
     
-    t_in = *t;
-    x_in = *x;
-    v_in = *v;
     
-    kx1 = dxdt(t_in, x_in, v_in);
-    kv1 = dvdt(t_in, x_in, v_in);    
-    kx2 = dxdt(t_in + dt/2, x_in + kx1 * dt/2, v_in + kv1 * dt/2);
-    kv2 = dvdt(t_in + dt/2, x_in + kx1 * dt/2, v_in + kv1 * dt/2);
-    kx3 = dxdt(t_in + dt/2, x_in + kx2 * dt/2, v_in + kv2 * dt/2);
-    kv3 = dvdt(t_in + dt/2, x_in + kx2 * dt/2, v_in + kv2 * dt/2);
-    kx4 = dxdt(t_in + dt, x_in + kx3 * dt, v_in+ kv3 *dt);
-    kv4 = dvdt(t_in + dt, x_in + kx3 * dt, v_in+ kv3 *dt);
+    for (int i = 1; i<= punt; i++)
+    {
+        kx1 = d*dxdt(t[i-1], x[i-1], v[i-1]);
+        kv1 = d*dvdt(t[i-1], x[i-1], v[i-1]);
+        
+        kx2 = d*dxdt(t[i-1] + 0.5*d, x[i-1]+0.5*kx1, v[i-1]+0.5*kv1);
+        kv2 = d*dvdt(t[i-1] + 0.5*d, x[i-1]+0.5*kx1, v[i-1]+0.5*kv1);
+        
+        kx3 = d*dxdt(t[i-1] + 0.5*d, x[i-1]+0.5*kx2, v[i-1]+0.5*kv2);
+        kv3 = d*dvdt(t[i-1] + 0.5*d, x[i-1]+0.5*kx2, v[i-1]+0.5*kv2);
+        
+        kx4 = d*dxdt(t[i-1]+ d, x[i-1] +kx3, v[i-1]+kv3);
+        kv4 = d*dvdt(t[i-1]+ d, x[i-1] +kx3, v[i-1]+kv3);
+
+    }
     
-    t_in = t_in + dt;
-    x_in = x_in + dt * (kx1 + 2*kx2 + 2*kx3 + kx4)*(1.0/6.0);
-    v_in = v_in + dt + (kv1 + 2+kv2 + 2*kv3 + kv4)*(1.0/6.0);
-    
-    *t = t_in;
-    *x = x_in;
-    *v = v_in;
 }
 
 
@@ -53,10 +49,11 @@ int main()
 {
     int t = 0;
     int tf = 5;
-    float x = 0.1;
-    float v = 0;
     float dt = 0.01;
     float puntos = (tf-t)/dt;
+    float x[puntos];
+    float v[puntos];
+    float t[puntos];
     
     for (int i = 1; i<= puntos; i++)
     {
