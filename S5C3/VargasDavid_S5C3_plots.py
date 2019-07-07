a9b11658c5f857691e27bfe69b8539b3fcd33eff
davidv4rg <id.vargas@uniandes.edu.co>
2019-07-07 21:37:20 +0000

    programa para plotear los datos

--- S5C3/VargasDavid_S5C3_plots.py
+++ S5C3/VargasDavid_S5C3_plots.py
@@ -5,10 +5,29 @@ datos = np.genfromtxt("data.dat", delimiter = ";")
 
 t = datos[:,0]
 x = datos[:,1]
+v = datos[:,2]
 
-plt.figure(figsize= (10,5))
+plt.figure(figsize= (10,13))
+
+
+plt.subplot(3,1,1)
 plt.plot(t,x, c="crimson", linewidth = 1.5)
 plt.ylabel("$Posición$ $(m)$")
-plt.xlabel("$Tiempo$ $(s)$" )
+plt.xlabel("$Tiempo$ $(s)$")
+
+plt.subplot(3,1,2)
+plt.plot(t,v, c="darkolivegreen", linewidth = 1.5)
+plt.ylabel("$Velocidad$ $(m/s)$")
+plt.xlabel("$Tiempo$ $(s)$")
+
+plt.subplot(3,1,3)
+plt.plot(t,x, c="crimson", linewidth = 1.5, label = "$Posición$")
+plt.plot(t,v, c="darkolivegreen", linewidth = 1.5, ls = "--", label="$Velocidad$")
+plt.xlabel("$Tiempo$ $(s)$")
+plt.legend()
+
+
 plt.savefig("VargasDavidResorte.pdf")
+
+
 
