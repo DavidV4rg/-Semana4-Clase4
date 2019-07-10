ad61ce519cdef10ff9140f7878d6dbe648efec92
Default user <id.vargas@uniandes.edu.co>
2019-07-10 23:57:44 +0000

    condiciones extremnos fijos

--- S6C3/VargasDavid_S6C3.cpp
+++ S6C3/VargasDavid_S6C3.cpp
@@ -12,7 +12,7 @@ int main()
     float c = 300;
     float l = 1.0;
     float A0 = 0.1;
-    int N_puntos = (l/dx)+1;
+    int N_puntos =(l/dx)+1;
     float dt = (dx*0.9)/c;
     float xpast[N_puntos], xpres[N_puntos], xfutu[N_puntos], x[N_puntos];
     float t[N_puntos];        
@@ -20,12 +20,15 @@ int main()
     t[N_puntos] = l;
     x[0] = 0;
     xpast[0] = 0;
+    xpast[N_puntos] = 0;
     xpres[0] = 0;
+    xpres[N_puntos] = 0;
     xfutu[0] = 0;
+    xfutu[N_puntos] = 0;
     double r = pow((c*dt)/dx,2);    
 
     ofstream outfile;
-    outfile.open("inicia.dat");
+    outfile.open("inicial.dat");
     //Condiciones iniciales
     for (int i = 1; i<N_puntos+1; i++)
     {
@@ -42,35 +45,34 @@ int main()
             x[i] = -(2*A0/l)*t[i] + 2*A0;
             xpast[i] = x[i];
         }
-        outfile << t[i-1] << ";" << xpast[i-1]<<endl;
+        
     }
-    outfile.close();
+
     //Primer paso
     
-    ofstream outfile2;
-    outfile2.open("primer.dat");
+
         
-    for (int i = 0; i <=N_puntos; i++)
+    for (int i = 1; i <=N_puntos; i++)
     {
         xpres[i] = (r/2)*(xpast[i+1]+xpast[i-1]-2*xpast[i]) + xpast[i]; 
-        outfile2 << xpres[i]<< endl;
+        outfile << t[i-1] << ";" << xpast[i-1]<<";"<< xpres[i-1] <<endl;
     }
-    outfile2.close();
+    outfile.close();
     
     //Siguientes pasos
     int contador = 0;
     ofstream outfile1;
     outfile1.open("data.dat");
-    for (int i = 0; i<=N_puntos; i++)
+    for (int i = 0; i<=N_puntos+250; i++)
     {
         contador = contador+1;
-        for (int k = 1; k<N_puntos; k++)
+        for (int k = 1; k <= N_puntos; k++)
         {
             xfutu[k]= r*(xpres[k+1]+xpres[k-1]-2*xpres[k])-xpast[k]+2*xpres[k];
             
-            if(contador%100==0)
+            if(contador%15==0)
             {
-                outfile << t[k-1] <<";" << xfutu[k-1] <<endl;
+                outfile1 << xfutu[k-1] <<";"<< t[k-1] <<endl;
             }
         }
             
