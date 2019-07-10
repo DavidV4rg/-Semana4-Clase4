565b70fb7ff56da4ccaa08dc706d2339cf1841ec
Default user <id.vargas@uniandes.edu.co>
2019-07-10 03:56:56 +0000

    caso1

---
+++ S6C2/Plots_cuerda.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+datos = np.genfromtxt("data.dat", delimiter = ";")
+dato = np.genfromtxt("data1.dat", delimiter = ";")
+
+t = datos[:,0]
+x = datos[:,1]
+
+t1 = dato[:,0]
+x1 = dato[:,1]
+
+plt.figure(figsize=(10,7))
+plt.subplot(2,1,1)
+plt.xlabel("$Tiempo$ $(s)$")
+plt.ylabel("$Posición$ $(m)$")
+plt.plot(t,x, c= "g")
+
+plt.subplot(2,1,2)
+plt.xlabel("$Tiempo$ $(s)$")
+plt.ylabel("$Posición$ $(m)$")
+plt.plot(t1,x1, c="r")
+plt.savefig("cuerda.pdf")
+
