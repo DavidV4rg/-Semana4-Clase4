d6314479cd8ee11c376fab9df3f323ca01290ade
Default user <id.vargas@uniandes.edu.co>
2019-07-10 04:13:22 +0000

    caso 1

--- S6C2/Plots_cuerda.py
+++ S6C2/Plots_cuerda.py
@@ -13,11 +13,13 @@ x1 = dato[:,1]
 
 plt.figure(figsize=(10,7))
 plt.subplot(2,1,1)
+plt.grid()
 plt.xlabel("$Tiempo$ $(s)$")
 plt.ylabel("$Posición$ $(m)$")
 plt.plot(t,x, c= "g")
 
 plt.subplot(2,1,2)
+plt.grid()
 plt.xlabel("$Tiempo$ $(s)$")
 plt.ylabel("$Posición$ $(m)$")
 plt.plot(t1,x1, c="r")
