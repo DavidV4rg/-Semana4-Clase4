67419e8d675d4fe53d0d8599632c3ac5f0cbefaf
Default user <id.vargas@uniandes.edu.co>
2019-07-11 19:49:48 +0000

    se agrega lognorm

--- S6C4/plots_difusion.py
+++ S6C4/plots_difusion.py
@@ -1,24 +1,21 @@
 import numpy as np
 import matplotlib.pylab as plt
+from matplotlib.colors import LogNorm
 
 placa = np.loadtxt("data.txt")
 
 plt.figure()
 plt.imshow(placa)
+plt.colorbar()
 plt.savefig("placa")
 
-#placa1 = np.loadtxt("t0.txt")
-#placa2 = np.loadtxt("t1.txt")
-#placa3 = np.loadtxt("t2.txt")
+placa1 = np.loadtxt("T0.txt")
+placa2 = np.loadtxt("T1.txt")
 
-#plt.figure()
-#plt.imshow(placa1)
-#plt.savefig("placa1")
 
-#plt.figure()
-#plt.imshow(placa2)
-#plt.savefig("placa2")
+plt.figure()
+plt.imshow(placa1, norm = LogNorm())
+plt.colorbar()
+plt.savefig("placa1")
 
-#plt.figure()
-#plt.imshow(placa3)
-#plt.savefig("placa3")
+
