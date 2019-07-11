18ba6752aaf102082cd02d697f19840462d3ca45
Default user <id.vargas@uniandes.edu.co>
2019-07-11 00:16:54 +0000

    se hacen subplots

--- S6C3/Plots_cuerda.py
+++ S6C3/Plots_cuerda.py
@@ -11,12 +11,28 @@ pos1= dato[:,2]
 ti = datos[:,1]
 x = datos[:,0]
 
-plt.figure(figsize=(15,10))
+plt.figure(figsize=(30,14))
+
+plt.subplot(1,3,1)
+plt.plot(tiempo, pos, label="inicial", c="r")
 plt.grid()
 plt.xlabel("$Longitud$ $(m)$")
 plt.ylabel("$Amplitud$ $(m)$")
-plt.plot(tiempo, pos, label="inicial", c="r")
-plt.plot(tiempo, pos1, label ="primer paso", c = "g")
-plt.plot(ti, x, label= "siguientes pasos", c = "y")
 plt.legend()
+
+plt.subplot(1,3,2)
+plt.plot(tiempo, pos1, label ="primer paso", c = "g")
+plt.grid()
+plt.xlabel("$Longitud$ $(m)$")
+plt.ylabel("$Amplitud$ $(m)$")
+plt.legend()
+
+plt.subplot(1,3,3)
+plt.plot(ti, x, label= "siguientes pasos", c = "y")
+plt.grid()
+plt.xlabel("$Longitud$ $(m)$")
+plt.ylabel("$Amplitud$ $(m)$")
+plt.legend()
+
+
 plt.savefig("cuerda.pdf")
