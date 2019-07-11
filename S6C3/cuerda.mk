298d0179090a11d55bcf497a4e959f0d8a92da7d
Default user <id.vargas@uniandes.edu.co>
2019-07-11 00:00:39 +0000

    extremo fijo

--- S6C3/cuerda.mk
+++ S6C3/cuerda.mk
@@ -1,6 +1,7 @@
-cuerda.png: Plots_cuerda.py data.dat
+cuerda.pdf: Plots_cuerda.py data.dat inicial.dat
 	python Plots_cuerda.py
 data.dat: ./a.out
+inicial.dat: ./a.out
 	./a.out
 ./a.out: VargasDavid_S6C3.cpp
 	g++ VargasDavid_S6C3.cpp
