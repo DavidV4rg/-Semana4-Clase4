b6031fdca6be4f7b34e9ddb88ab2c1bd2688b982
Default user <id.vargas@uniandes.edu.co>
2019-07-12 16:07:29 +0000

    Finalmente funciona caso 1

--- S6C4/Difusion.cpp
+++ S6C4/Difusion.cpp
@@ -7,11 +7,11 @@ using namespace std;
 int main()
 {
     float dx = 0.01;
-    float v = pow(10,-8);
+    float v = pow(10,-4);
     float l = 1.0;
     float t0 = 0.0;
     float tf = 2500;
-    float dt = (dx*0.000025)/v;
+    float dt = 0.05;
     float temp0 = 50.0;
     float tempf = 100.0;
     int puntos = l/dx;
@@ -58,23 +58,23 @@ int main()
     ofstream outfile1;
     outfile1.open("Tiempo0.txt");
 
-    for(int i = 1; i <100; i+=dt)
+    for(float i = 0; i <100; i+=dt)
     {
-        for(int k = 1; k<puntos-1; k++)
+        for(int j = 1; j<puntos-1; j++)
         {
-            for (int j = 1; j<puntos-1; j++)
+            for (int k = 1; k<puntos-1; k++)
             {
-                T_fut[j][k] = constante*(T[j+1][k]+T[j-1][k]-2*T[j][k])+ constante*(T[j][k+1]+T[j][k-1]-2*T[j][k]) + T[j][k];
-                T[j][k] = T_fut[j][k];                
+                T_fut[k][j] = constante*(T[k+1][j]+T[k-1][j]-2*T[k][j])+ constante*(T[k][j+1]+T[k][j-1]-2*T[k][j]) + T[k][j];
+                T[k][j] = T_fut[k][j];                
             }
         }        
     }
     
-    for (int k=0; k<puntos;k++)
+    for (int j=0; j<puntos;j++)
     {
-        for (int j = 0; j<puntos; j++)
+        for (int k = 0; k<puntos; k++)
         {
-            outfile1 << T_fut[j][k] << " ";
+            outfile1 << T[k][j] << " ";
         }
         outfile1 << "\n";
     }
@@ -85,22 +85,22 @@ int main()
   
     ofstream outfile2;
     outfile2.open("Temp1.txt");
-    for(int i = 1; i <2500; i+=dt)
+    for(float i = 0; i <2500; i+=dt)
     {
-        for(int k = 1; k<puntos-1; k++)
+        for(int j = 1; j<puntos-1; j++)
         {
-            for (int j = 1; j<puntos-1; j++)
+            for (int k = 1; k<puntos-1; k++)
             {
-                T_fut[j][k] = constante*(T[j+1][k]+T[j-1][k]-2*T[j][k])+ constante*(T[j][k+1]+T[j][k-1]-2*T[j][k]) + T[j][k];                
-                T[j][k] = T_fut[j][k];
+                T_fut[k][j] = constante*(T[k+1][j]+T[k-1][j]-2*T[k][j])+ constante*(T[k][j+1]+T[k][j-1]-2*T[k][j]) + T[k][j];               
+                T[k][j] = T_fut[k][j];
             }        
         }
     }
-    for (int k=0; k<puntos;k++)
+    for (int j=0; j<puntos;j++)
     {
-        for (int j = 0; j<puntos; j++)
+        for (int k = 0; k<puntos; k++)
         {
-            outfile2 << T_fut[j][k] << " ";
+            outfile2 << T[k][j] << " ";
         }
         outfile2 << "\n";
     }
