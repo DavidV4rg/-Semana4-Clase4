ad2749a2d8b9cd3e65656ae72ef145c7668a217d
Default user <id.vargas@uniandes.edu.co>
2019-07-12 05:28:41 +0000

    caso 1 done

--- S6C4/Difusion.cpp
+++ S6C4/Difusion.cpp
@@ -11,20 +11,20 @@ int main()
     float l = 1.0;
     float t0 = 0.0;
     float tf = 2500;
-    float dt = (dx*0.00025)/v;
+    float dt = (dx*0.000025)/v;
     float temp0 = 50.0;
     float tempf = 100.0;
     int puntos = l/dx;
     float deltax = pow(dx,2);
+    cout<< "mi dt es: " << dt <<endl;
+    cout<< "numero de puntos es: "<<puntos<<endl;
     
     float T[puntos][puntos];
     float T_fut[puntos][puntos];
-    float time[puntos];
     
     //Condiciones iniciales
     
-    time[0] = 0;
-    time[puntos] = 2500;
+
     
     ofstream outfile;
     outfile.open("data.txt");
@@ -52,53 +52,60 @@ int main()
     }
     outfile.close();
     
-    //Calculo de la temperatura en el futuro (tiempo = 100s)
+    //Cálculo de la temperatura en el futuro (tiempo = 100s)
+    float constante = v*dt/deltax;
     
     ofstream outfile1;
-    outfile1.open("T1.txt");
-    for(int i = 1; i <=puntos+1; i++)
+    outfile1.open("Tiempo0.txt");
+
+    for(int i = 1; i <100; i+=dt)
     {
-        time[i] = time[i-1]+dt;
-        for(int k = 0; k<puntos; k++)
+        for(int k = 1; k<puntos-1; k++)
         {
-            for (int j = 0; j<puntos; j++)
+            for (int j = 1; j<puntos-1; j++)
             {
-                T_fut[j][k] = (v*dt/deltax)*(T[j+1][k]+T[j-1][k]-2*T[j][k])+ (v*dt/deltax)*(T[j][k+1]+T[j][k-1]-2*T[j][k]) + T[j][k];
-                if(time[i] == 100)
-                { 
-                    outfile1 << T_fut[j][k] << " ";
-                }
+                T_fut[j][k] = constante*(T[j+1][k]+T[j-1][k]-2*T[j][k])+ constante*(T[j][k+1]+T[j][k-1]-2*T[j][k]) + T[j][k];
+                T[j][k] = T_fut[j][k];                
             }
-            outfile1 << "\n";            
-        }  
-        outfile1.close();
+        }        
     }
+    
+    for (int k=0; k<puntos;k++)
+    {
+        for (int j = 0; j<puntos; j++)
+        {
+            outfile1 << T_fut[j][k] << " ";
+        }
+        outfile1 << "\n";
+    }
+    outfile1.close();
     
     
     //Calculo de la temperatura en el futuro (tiempo = 2500s)
-/*    
+  
     ofstream outfile2;
-    outfile2.open("T2.txt");
-    for(int i = 1; i <=puntos+1; i++)
+    outfile2.open("Temp1.txt");
+    for(int i = 1; i <2500; i+=dt)
     {
-        time[i] = time[i-1]+dt;
-        for(int k = 0; k<puntos; k++)
+        for(int k = 1; k<puntos-1; k++)
         {
-            for (int j = 0; j<puntos; j++)
+            for (int j = 1; j<puntos-1; j++)
             {
-                T_fut[j][k] = (v*dt/deltax)*(T[j+1][k]+T[j-1][k]-2*T[j][k])+ (v*dt/deltax)*(T[j][k+1]+T[j][k-1]-2*T[j][k]) + T[j][k];
-                if(time[i] == 2500)
-                { 
-                    outfile2 << T_fut[j][k] << " ";
-                }
-            }
-            outfile2 << "\n";            
+                T_fut[j][k] = constante*(T[j+1][k]+T[j-1][k]-2*T[j][k])+ constante*(T[j][k+1]+T[j][k-1]-2*T[j][k]) + T[j][k];                
+                T[j][k] = T_fut[j][k];
+            }        
         }
-        outfile2.close();
     }
+    for (int k=0; k<puntos;k++)
+    {
+        for (int j = 0; j<puntos; j++)
+        {
+            outfile2 << T_fut[j][k] << " ";
+        }
+        outfile2 << "\n";
+    }
+    outfile2.close();
     
-    
-*/    
     
     return 0;
 }
