20021aa51e5125667504b3ce184410a116261d5c
Default user <id.vargas@uniandes.edu.co>
2019-07-12 05:29:17 +0000

    graficas

--- S6C4/plots_difusion.py
+++ S6C4/plots_difusion.py
@@ -1,21 +1,24 @@
 import numpy as np
 import matplotlib.pylab as plt
-from matplotlib.colors import LogNorm
 
 placa = np.loadtxt("data.txt")
+placa1 = np.loadtxt("Tiempo0.txt")
+placa2 = np.loadtxt("Temp1.txt")
 
-plt.figure()
+plt.figure(figsize=(15,10))
+
+
+plt.subplot(1,3,1)
 plt.imshow(placa)
-plt.colorbar()
-plt.savefig("placa")
+plt.title("$Tiempo$ $0s$")
 
-placa1 = np.loadtxt("T0.txt")
-placa2 = np.loadtxt("T1.txt")
+plt.subplot(1,3,2)
+plt.imshow(placa1)
+plt.title("$Tiempo$ $100s$")
 
-
-plt.figure()
-plt.imshow(placa1, norm = LogNorm())
-plt.colorbar()
-plt.savefig("placa1")
+plt.subplot(1,3,3)
+plt.imshow(placa2)
+plt.title("$Tiempo$ $2500s$")
 
 
+plt.savefig("placa.pdf")
