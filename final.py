2d32490b2cbb68925886a34858830d4d7281d5cd
Default user <id.vargas@uniandes.edu.co>
2019-07-22 20:44:00 +0000

    repaso

--- final.py
+++ final.py
@@ -94,13 +94,9 @@ def ITF(señal):
 inversa = ITF(tra)
 
 plt.figure()
-plt.plot(t, inversa)
+plt.plot(t, inversa) 
 
-# Ejercicio 4
-# Resuelva el siguiente sistema acoplado de ecuaciones diferenciales 
-# dx/dt = sigma * (y - x)
-# dy/dt = rho * x - y -x*z
-# dz/dt = -beta * z + x * y
-# con sigma = 10, beta=2.67, rho=28.0,
-# condiciones iniciales t=0, x=0.5, y=-1.0, z=1.0, hasta t=5.0.
-# Prepare dos graficas con la solucion: de x vs y (xy.png), x vs. z (xz.png) 
+
+
+
+
